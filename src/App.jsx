6f6d820b031083a4950e8abd7fe69c1a7182d171
phock12 <perrinhock12@gmail.com>
2026-08-06 06:34:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Favorites from "./pages/Favorites";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Favorites" element={<Favorites />} />
-        </Routes>
+      <div className="app-container">      //Define New Class app-container
+
+        <div className="navbar-wrapper">   //Navbar section 
+          <Navbar />
+        </div>
+
+
+        <div className="page-wrapper">     //
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Favorites" element={<Favorites />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
