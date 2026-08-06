c12f93c51de49e56248d5daf0babcdcb19677cb6
phock12 <perrinhock12@gmail.com>
2026-08-06 07:02:05 -0500

    Update
    Added dark mode functionality with local storage support.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Favorites from "./pages/Favorites";
-import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+    document.body.classList.toggle("dark-mode", darkMode);
+  }, [darkMode]);
+
   return (
     <BrowserRouter>
-      <div className="app-container">      //Define New Class app-container
+      <div className="app-container">
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
 
-        <div className="navbar-wrapper">   //Navbar section 
-          <Navbar />
-        </div>
-
-
-        <div className="page-wrapper">     //
+        <div className="page-wrapper">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Favorites" element={<Favorites />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/favorites" element={<Favorites />} />
           </Routes>
         </div>
       </div>
